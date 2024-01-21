f9c057736fa60a5bf329fe131fb87ab3d727c0d1
Aman Rawat <rawataman101@gmail.com>
2024-01-21 10:47:06 +0530

    C4: Search Bar

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./Logo.module.css";
 import LogoImage from "../../assets/qtify_logo.png";
 function Logo() {
   return <img src={LogoImage} alt="logo" width={67} />;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,14 +1,20 @@
 import React from "react";
-import SearchIcon from "@mui/icons-material/Search";
+import { ReactComponent as SearchIcon } from "../../assets/search_icon.svg";
 import styles from "./Searchbar.module.css";
-function Searchbar() {
+function Searchbar({ placeholder }) {
   const onSubmit = (e) => {
     e.preventDefault();
   };
   return (
-    <form className="styles.wrapper" onSubmit={onSubmit}>
-      <input placeholder={placeholder} required />
-      <button type="submit">Search</button>
+    <form className={styles.searchbar} onSubmit={onSubmit}>
+      <input
+        className={styles.searchbar__input}
+        placeholder={placeholder}
+        required
+      />
+      <button className={styles.searchbar__button} type="submit">
+        <SearchIcon />
+      </button>
     </form>
   );
 }
